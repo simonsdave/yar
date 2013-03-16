3b7c53e3ac4c18a243b966149989432efd7f05ff
Dave Simons <simonsdave@gmail.com>
2013-03-16 18:26:49 -0400

    Adding command line port option and start of accepting requests for MAC crednetials. Dave.

--- keyserver/keyserver.py
+++ keyserver/keyserver.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
 
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, world")
+from tornado.options import define, options
+define("port", default=8000, help="run on the given port", type=int)
 
-application = tornado.web.Application([
-	(r"/.*", MainHandler),
-])
+class MACCredssHandler(tornado.web.RequestHandler):
+	def get(self):
+		self.write("creds")
+
+class MACCredsHandler(tornado.web.RequestHandler):
+	def get(self,key):
+		self.write(">>>%s<<<" % key)
 
 if __name__ == "__main__":
-	application.listen(8888)
+	tornado.options.parse_command_line()
+	handlers = [
+		(r"/mac_creds", MACCredssHandler),
+		(r"/mac_creds/(.*)", MACCredsHandler)
+	]
+	app = tornado.web.Application(handlers=handlers)
+	http_server = tornado.httpserver.HTTPServer(app)
+	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
