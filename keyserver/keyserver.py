43933f69300150b0d54f736dfdd2b53d11b84d35
Dave Simons <simonsdave@gmail.com>
2013-03-19 21:50:53 -0400

    Initial cut at getting DELETE to work mostly correctly. Dave.

--- keyserver/keyserver.py
+++ keyserver/keyserver.py
@@ -79,21 +79,39 @@ class CredsHandler(tornado.web.RequestHandler):
 
 	# curl -v -X GET http://localhost:6969/mac_creds/b205c21fbc467b4d28aa93fba7000d12
 	def get(self,id):
-		http_client = tornado.httpclient.HTTPClient()
-		# :TODO: should not be hardcoded
-		url_fmt = 'http://localhost:5984/macaa/_design/creds/_view/all?startkey="%s"&endkey="%s"'
-		url = url_fmt % (id, id)
-		response = http_client.fetch(url)
-		body = json.loads(response.body)
-		rows = body['rows']
-		if 0 == len(rows):
+		value = self._get(id)
+		if value is None:
 			self.clear()
 			self.set_status(404)
 		else:
-			row = rows[0]
-			value = row['value']
 			# since value is a dict self.write() will correctly set the content type
 			self.write(value)
+
+	# curl -v -X DELETE http://localhost:6969/mac_creds/b205c21fbc467b4d28aa93fba7000d12
+	def delete(self,id):
+		doc = self._get(id)
+		if doc is None:
+			self.clear()
+			self.set_status(404)
+		else:
+			doc["is_deleted"] = True
+			http_client = tornado.httpclient.HTTPClient()
+			response = http_client.fetch(
+				"http://localhost:5984/macaa/%s" % id,	# :TODO: should not be hardcoded
+				method="PUT",
+				headers={"Content-Type": "application/json; charset=utf8"},
+				body=json.dumps(doc)
+				)
+
+	def _get(self,id):
+		http_client = tornado.httpclient.HTTPClient()
+		# :TODO: should not be hardcoded
+		url = 'http://localhost:5984/macaa/%s' % id
+		# :TODO: deal with id not being found
+		response = http_client.fetch(url)
+		if response is None:
+			return None
+		return json.loads(response.body)
 
 if __name__ == "__main__":
 	tornado.options.parse_command_line()
