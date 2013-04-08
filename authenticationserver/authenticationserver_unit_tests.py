389e8982ba78b528fe0c5acf3675345502135f52
Dave Simons <simonsdave@gmail.com>
2013-04-08 07:16:53 -0400

    Starting to add some authorization server unit tests around testing valid & invalid Authorization HTTP header. Dave.

--- authenticationserver/authenticationserver_unit_tests.py
+++ authenticationserver/authenticationserver_unit_tests.py
@@ -137,6 +137,14 @@ class TestWhatever(AuthenticationServerTestCase):
 		response, content = http_client.request(self.url(), "GET")
 		self.assertTrue(response.status == httplib.UNAUTHORIZED)
 
+	def test_get_with_invalid_authorization_header(self):
+		http_client = httplib2.Http()
+		response, content = http_client.request(
+			self.url(),
+			"GET",
+			headers={"Authorization": 'MAC id="", nonce="98", mac="bindle"'})
+		self.assertTrue(response.status == httplib.UNAUTHORIZED)
+
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
