f59cfa9b49e475e84ab8922bd9ce23ba6ab52f2e
Dave Simons <simonsdave@gmail.com>
2013-04-08 13:26:13 -0400

    Now we're starting to make some progress on converting the KeyStore install to a python script instead of a series of bash scripts. A lot easier and will make testing easier. Dave.

--- keystore/keystore.py
+++ keystore/keystore.py
@@ -5,6 +5,9 @@
 #
 #-------------------------------------------------------------------------------
 
+import os
+import sys
+import glob
 import httplib
 import httplib2
 
@@ -19,31 +22,36 @@ default_db = "macaa"
 def create(db=default_db,host=default_host,port=default_port):
 
 	url = "http://%s:%d/%s" % (host, port, db)
-
 	http_client = httplib2.Http()
 	response, content = http_client.request(url,"PUT")
-	if httplib.CREATED == response.status:
-		print "Successfully created database '%s' on '%s:%d'." % (
-			db, host, port)
-	else:
-		print "Failed to create database '%s' on '%s:%d'." % (
-			db, host, port)
+	if httplib.CREATED != response.status:
+		print "Failed to create database '%s' on '%s:%d'." % (db, host, port)
+		return False
+	print "Successfully created database '%s' on '%s:%d'." % (db, host, port)
 
-	design_doc = None
-	with open("keystore.json", "r") as f:
-		design_doc = f.read()
-	print design_doc
+	path = os.path.split(sys.argv[0])[0]
+	design_doc_file_name_pattern = os.path.join(path,"*.json")
+	for design_doc_file_name in glob.glob(design_doc_file_name_pattern):
 
-	http_client = httplib2.Http()
-	response, content = http_client.request(url,"PUT")
-	if httplib.CREATED == response.status:
-		print "Successfully created database '%s' on '%s:%d'." % (
-			db, host, port)
-	else:
-		print "Failed to create database '%s' on '%s:%d'." % (
-			db, host, port)
+		design_doc_name = os.path.basename(design_doc_file_name)[:-len(".json")]
 
-# headers={"Content-Type": "application/json; charset=utf8"}
+		with open(design_doc_file_name, "r") as design_doc_file:
+			design_doc = design_doc_file.read()
+
+		url = "http://%s:%d/%s/_design/%s" % (host, port, db, design_doc_name)
+		http_client = httplib2.Http()
+		response, content = http_client.request(
+			url,
+			"PUT",
+			body=design_doc,
+			headers={"Content-Type": "application/json; charset=utf8"})
+		if httplib.CREATED != response.status:
+			print "Failed to create design doc '%s'" % url
+			return False
+		print "Successfully created design doc '%s'" % url
+
+	return True
+
 #-------------------------------------------------------------------------------
 
 def delete(db=default_db,host=default_host,port=default_port):
@@ -61,8 +69,7 @@ def delete(db=default_db,host=default_host,port=default_port):
 #-------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-	print __file__
-	# delete()
-	# create()
+	delete()
+	create()
 
 #------------------------------------------------------------------- End-of-File
