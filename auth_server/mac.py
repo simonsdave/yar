4b41d60a599e910d5c6c4abee0878f40818f48e2
Dave Simons <simonsdave@gmail.com>
2013-04-15 20:32:36 -0400

    First cut at a rough outline of a class to calculate the auth server's MAC. Dave.

--- auth_server/mac.py
+++ auth_server/mac.py
@@ -10,6 +10,10 @@ import logging
 logging.basicConfig(level=logging.FATAL)
 import string
 import random
+import datetime
+import hashlib
+import hmac
+import base64
 
 #-------------------------------------------------------------------------------
 
@@ -39,4 +43,66 @@ class Nonce(str):
 		rstr = ''.join(random.choice(chars) for i in range(nonce_len))
 		return str.__new__(self, rstr)
 
+#-------------------------------------------------------------------------------
+
+class Timestamp(object):
+	"""..."""
+
+	def __init__(self):
+		epoch = datetime.datetime(1970, 1, 1, 0, 0, 0)
+		self._ts = int((datetime.datetime.utcnow() - epoch).total_seconds())
+
+	def __str__(self):
+		return str(self._ts)
+
+#-------------------------------------------------------------------------------
+
+class Mac(object):
+	"""..."""
+
+	def __init__(
+		self,
+		mac_key,
+		mac_algorithm,
+		ts,
+		nonce,
+		http_method,
+		uri,
+		host,
+		port,
+		content_type = None,
+		md5_of_body = None):
+
+		assert mac_algorithm == "hmac-sha-1" or mac_algorithm == "hmac-sha-256"
+
+		if mac_algorithm == "hmac-sha-1":
+			self._mac_algorithm = hashlib.sha1
+		else:
+			self._mac_algorithm = hashlib.sha256
+
+		assert (content_type is None and md5_of_body is None) or \
+			   (content_type is not None and md5_of_body is not None)
+
+		if content_type is None:
+			self._ext = ""
+		else:
+			self._ext = "%s-%s" % (content_type, md5_of_body or "")
+
+		self._normalized_request_string = str(ts) + '\n' + \
+			str(nonce) + '\n' + \
+			http_method + '\n' + \
+			uri + '\n' + \
+			host + '\n' + \
+			str(port) + '\n' + \
+			self._ext + '\n'
+
+		self._hmac = hmac.new(
+			mac_key,
+			self._normalized_request_string,
+			self._mac_algorithm)
+		self._base64_encoded_hmac = base64.b64encode( self._hmac.digest() )
+
+	def __str__(self):
+		return self._base64_encoded_hmac
+			
 #------------------------------------------------------------------- End-of-File
