4fd44301a8882bc145780c45f21c3f27e7a9f409
Dave Simons <simonsdave@gmail.com>
2013-05-08 17:55:34 -0400

    Adding a pretty simple module and unit tests with some generally useful string utilities. Dave.

---
+++ lib/strutil.py
@@ -0,0 +1,20 @@
+"""This module contains a series of utilities for manipulating valueings."""
+
+#-------------------------------------------------------------------------------
+
+def make_http_header_value_friendly(value):
+	"""The caller wants to return ```value``` in an HTTP response
+	where ```value``` is the value for an HTTP header. This won't work
+	so well if ```value``` contains things like tabs and newlines.
+	This function creates a version of ```value``` that can safely
+	be used as the value for an HTTP header."""
+	if value is None:
+		return "<None>"
+	if value is not str:
+		value = str(value)
+	value = value.replace('\n','\\n')
+	value = value.replace('\r','\\r')
+	value = value.replace('\t','\\t')
+	return value
+
+#------------------------------------------------------------------- End-of-File
---
+++ lib/strutil_unit_tests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-------------------------------------------------------------------------------
+#
+# strutil_unit_tests.py
+#
+#-------------------------------------------------------------------------------
+
+import unittest
+
+import strutil
+
+#-------------------------------------------------------------------------------
+
+class MakeHttpHeaderValueTestCase(unittest.TestCase):
+	
+	def test_value_is_none(self):
+		v = None
+		hv = strutil.make_http_header_value_friendly(v)
+		self.assertIsNotNone(hv)
+		self.assertEqual(hv, "<None>")
+
+	def test_value_is_not_a_string(self):
+		v = 1
+		hv = strutil.make_http_header_value_friendly(v)
+		self.assertIsNotNone(hv)
+		self.assertEqual(hv, str(v))
+
+	def test_value_with_tabs_and_newline(self):
+		v = "dave\t\nwas"
+		hv = strutil.make_http_header_value_friendly(v)
+		self.assertIsNotNone(hv)
+		self.assertEqual(hv, "dave\\t\\nwas")
+
+#-------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+	unittest.main()
+
+#------------------------------------------------------------------- End-of-File
+
