2440c0699f6efcaea706427dd04da3d31e3ce20c
Dave Simons <simonsdave@gmail.com>
2013-05-07 20:02:08 -0400

    Good start at taking secpost.sh and generalizing it for all HTTP methods. Dave.

--- bin/secpost.sh
+++ bin/secpost.sh
@@ -1,41 +1,67 @@
 #!/bin/bash -xv
 
-if [ $# != 2 ]; then
-    echo "usage: `basename $0` <mac key identifier> <uri>"
-    exit 1
+usage_and_exit() {
+	echo "usage: `basename $0` [GET|POST|PUT|DELETE] <mac key identifier> <uri>"
+	exit 1
+}
+
+if [ $# != 3 ]; then
+	usage_and_exit
 fi
 
 SCRIPTDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-URI=$2
+HTTP_METHOD=`echo $1 | awk '{print toupper($0)}'`
+MAC_KEY_IDENTIFIER=$2
+URI=$3
 
-MAC_KEY_IDENTIFIER=$1
+case "$HTTP_METHOD" in
+        GET)
+            ;;
+        POST)
+            ;;
+        DELETE)
+            ;;
+        PUT)
+            ;;
+        *)
+            usage_and_exit
+esac
+
 MAC_KEY=`$SCRIPTDIR/gmk.sh $MAC_KEY_IDENTIFIER`
 MAC_ALGORITHM=`$SCRIPTDIR/gma.sh $MAC_KEY_IDENTIFIER`
-CONTENT_TYPE="application/json; charset=utf-8"
-TMP_STDIN=`mktemp -t DAS`
-cat /dev/stdin > $TMP_STDIN
 HOST=localhost
 PORT=8000
-HTTP_METHOD=POST
-AUTH_HEADER_CMD="$SCRIPTDIR/genahv.py \
-	$HTTP_METHOD \
-	$HOST \
-	$PORT \
-	$URI \
-	$MAC_KEY_IDENTIFIER \
-	$MAC_KEY \
-	$MAC_ALGORITHM \
-	'$CONTENT_TYPE' < $TMP_STDIN"
-AUTH_HEADER_VALUE=`eval $AUTH_HEADER_CMD`
+
+if [ "POST" == "$HTTP_METHOD" ]; then
+	CONTENT_TYPE="application/json; charset=utf-8"
+	COPY_OF_STDIN=`mktemp -t DAS`
+	cat /dev/stdin > $COPY_OF_STDIN
+else
+	CONTENT_TYPE=""
+	COPY_OF_STDIN=/dev/null
+fi
+
+GEN_AUTH_HEADER_VALUE_CMD="$SCRIPTDIR/genahv.py \
+    $HTTP_METHOD \
+    $HOST \
+    $PORT \
+    $URI \
+    $MAC_KEY_IDENTIFIER \
+    $MAC_KEY \
+    $MAC_ALGORITHM"
+if [ "POST" == "$HTTP_METHOD" ]; then
+    GEN_AUTH_HEADER_VALUE_CMD="$GEN_AUTH_HEADER_VALUE_CMD '$CONTENT_TYPE' < $COPY_OF_STDIN"
+fi
+AUTH_HEADER_VALUE=`eval $GEN_AUTH_HEADER_VALUE_CMD`
 
 curl \
 	-s \
 	-v \
 	-X $HTTP_METHOD \
-	-H "Content-Type: $CONTENT_TYPE" \
 	-H "Authorization: $AUTH_HEADER_VALUE" \
-	--data-binary @$TMP_STDIN \
+	-H "Content-Type: $CONTENT_TYPE" \
+	--data-binary @$COPY_OF_STDIN \
    http://$HOST:$PORT"$URI"
 
 exit 0
