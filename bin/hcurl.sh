ee1c175ebbe01e2ab493c249f7c08f369395451b
Dave Simons <simonsdave@gmail.com>
2013-05-08 07:25:20 -0400

    Ooops ... removing debugging flags from hcurl script. Dave.

--- bin/hcurl.sh
+++ bin/hcurl.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -xv
+#!/bin/bash
 #-------------------------------------------------------------------------------
 #
 # A wrapper around cURL that adds an HMAC HTTP Authorization header
