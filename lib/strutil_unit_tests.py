3f2349226734857ed1990c210929ecdc7646e69e
Dave Simons <simonsdave@gmail.com>
2013-05-15 21:08:15 -0400

    pep8'ing lib/strutil_unit_tests.py

--- lib/strutil_unit_tests.py
+++ lib/strutil_unit_tests.py
@@ -1,31 +1,28 @@
 """This module contains a collection of unit tests which
 validate lib/strutil.py"""
 
+
 import unittest
 
 import strutil
 
-#-------------------------------------------------------------------------------
 
 class MakeHttpHeaderValueTestCase(unittest.TestCase):
-	
-	def test_value_is_none(self):
-		v = None
-		hv = strutil.make_http_header_value_friendly(v)
-		self.assertIsNotNone(hv)
-		self.assertEqual(hv, "<None>")
 
-	def test_value_is_not_a_string(self):
-		v = 1
-		hv = strutil.make_http_header_value_friendly(v)
-		self.assertIsNotNone(hv)
-		self.assertEqual(hv, str(v))
+    def test_value_is_none(self):
+        v = None
+        hv = strutil.make_http_header_value_friendly(v)
+        self.assertIsNotNone(hv)
+        self.assertEqual(hv, "<None>")
 
-	def test_value_with_tabs_and_newline(self):
-		v = "dave\t\nwas"
-		hv = strutil.make_http_header_value_friendly(v)
-		self.assertIsNotNone(hv)
-		self.assertEqual(hv, "dave\\t\\nwas")
+    def test_value_is_not_a_string(self):
+        v = 1
+        hv = strutil.make_http_header_value_friendly(v)
+        self.assertIsNotNone(hv)
+        self.assertEqual(hv, str(v))
 
-#------------------------------------------------------------------- End-of-File
-
+    def test_value_with_tabs_and_newline(self):
+        v = "dave\t\nwas"
+        hv = strutil.make_http_header_value_friendly(v)
+        self.assertIsNotNone(hv)
+        self.assertEqual(hv, "dave\\t\\nwas")
