6e1f5ba0a8f4bc8aab0efb8e85ccd0c0b57a3c55
Dave Simons <simonsdave@gmail.com>
2013-05-15 21:20:23 -0400

    pep8'ing lib/clparserutil_unit_tests.py

--- lib/clparserutil_unit_tests.py
+++ lib/clparserutil_unit_tests.py
@@ -6,142 +6,139 @@ import unittest
 
 import clparserutil
 
-#-------------------------------------------------------------------------------
 
 class TestCase(unittest.TestCase):
-	
-	def test_check_couchdb(self):
-		option = clparserutil.Option(
-			"--create",
-			action="store",
-			dest="couchdb",
-			default="bindle:8909/berry",
-			type="couchdb",
-			help="whatever")
-		values = [
-			["bindle:8909/berry", "bindle:8909/berry"],
-			["b:8/y", "b:8/y"],
 
-			["dave", None],
-			["dave:89", None],
-			["89/", None],
-			["dave:89/", None],
-		]
-		type_checker = clparserutil.Option.TYPE_CHECKER["couchdb"]
-		self.assertIsNotNone(type_checker)
-		opt_string = option.get_opt_string(),
-		for value in values:
-			if value[1] is not None:
-				msg = "Faild to parse '%s' correctly." % value[0]
-				result = type_checker(option, opt_string, value[0])
-				self.assertEqual(result, value[1], msg)
-			else:
-				with self.assertRaises(optparse.OptionValueError):
-					type_checker(option, opt_string, value[0])
+    def test_check_couchdb(self):
+        option = clparserutil.Option(
+            "--create",
+            action="store",
+            dest="couchdb",
+            default="bindle:8909/berry",
+            type="couchdb",
+            help="whatever")
+        values = [
+            ["bindle:8909/berry", "bindle:8909/berry"],
+            ["b:8/y", "b:8/y"],
 
-	def test_check_logginglevel(self):
-		option = clparserutil.Option(
-			"--create",
-			action="store",
-			dest="logging_level",
-			default=logging.FATAL,
-			type="logginglevel",
-			help="whatever")
-		values = [
-			["debug", logging.DEBUG],
-			["info", logging.INFO],
-			["INFO", logging.INFO],
-			["warning", logging.WARNING],
-			["eRRor", logging.ERROR],
-			["CRITICAL", logging.CRITICAL],
-			["FATAL", logging.FATAL],
+            ["dave", None],
+            ["dave:89", None],
+            ["89/", None],
+            ["dave:89/", None],
+        ]
+        type_checker = clparserutil.Option.TYPE_CHECKER["couchdb"]
+        self.assertIsNotNone(type_checker)
+        opt_string = option.get_opt_string(),
+        for value in values:
+            if value[1] is not None:
+                msg = "Faild to parse '%s' correctly." % value[0]
+                result = type_checker(option, opt_string, value[0])
+                self.assertEqual(result, value[1], msg)
+            else:
+                with self.assertRaises(optparse.OptionValueError):
+                    type_checker(option, opt_string, value[0])
 
-			["dave", None],
-			["None", None],
-			["", None],
-		]
-		type_checker = clparserutil.Option.TYPE_CHECKER["logginglevel"]
-		self.assertIsNotNone(type_checker)
-		opt_string = option.get_opt_string(),
-		for value in values:
-			if value[1] is not None:
-				msg = "Faild to parse '%s' correctly." % value[0]
-				result = type_checker(option, opt_string, value[0])
-				self.assertEqual(result, value[1], msg)
-			else:
-				with self.assertRaises(optparse.OptionValueError):
-					type_checker(option, opt_string, value[0])
+    def test_check_logginglevel(self):
+        option = clparserutil.Option(
+            "--create",
+            action="store",
+            dest="logging_level",
+            default=logging.FATAL,
+            type="logginglevel",
+            help="whatever")
+        values = [
+            ["debug", logging.DEBUG],
+            ["info", logging.INFO],
+            ["INFO", logging.INFO],
+            ["warning", logging.WARNING],
+            ["eRRor", logging.ERROR],
+            ["CRITICAL", logging.CRITICAL],
+            ["FATAL", logging.FATAL],
 
-	def test_check_host_colon_port(self):
-		option = clparserutil.Option(
-			"--create",
-			action="store",
-			dest="server",
-			default="bindle:8909",
-			type="hostcolonport",
-			help="whatever")
-		values = [
-			["bindle:8909", "bindle:8909"],
-			["b:8", "b:8"],
+            ["dave", None],
+            ["None", None],
+            ["", None],
+        ]
+        type_checker = clparserutil.Option.TYPE_CHECKER["logginglevel"]
+        self.assertIsNotNone(type_checker)
+        opt_string = option.get_opt_string(),
+        for value in values:
+            if value[1] is not None:
+                msg = "Faild to parse '%s' correctly." % value[0]
+                result = type_checker(option, opt_string, value[0])
+                self.assertEqual(result, value[1], msg)
+            else:
+                with self.assertRaises(optparse.OptionValueError):
+                    type_checker(option, opt_string, value[0])
 
-			["dave", None],
-			["89", None],
-			[":89", None],
-		]
-		type_checker = clparserutil.Option.TYPE_CHECKER["hostcolonport"]
-		self.assertIsNotNone(type_checker)
-		opt_string = option.get_opt_string(),
-		for value in values:
-			if value[1] is not None:
-				msg = "Faild to parse '%s' correctly." % value[0]
-				result = type_checker(option, opt_string, value[0])
-				self.assertEqual(result, value[1], msg)
-			else:
-				with self.assertRaises(optparse.OptionValueError):
-					type_checker(option, opt_string, value[0])
+    def test_check_host_colon_port(self):
+        option = clparserutil.Option(
+            "--create",
+            action="store",
+            dest="server",
+            default="bindle:8909",
+            type="hostcolonport",
+            help="whatever")
+        values = [
+            ["bindle:8909", "bindle:8909"],
+            ["b:8", "b:8"],
 
-	def test_check_boolean(self):
-		option = clparserutil.Option(
-			"--create",
-			action="store",
-			dest="create",
-			default=True,
-			type="boolean",
-			help="create key store - default = True")
-		values = [
-			["true", True],
-			["True", True],
-			["trUe", True],
-			["t", True],
-			["T", True],
-			["1", True],
-			["y", True],
-			["yes", True],
-			["y", True],
+            ["dave", None],
+            ["89", None],
+            [":89", None],
+        ]
+        type_checker = clparserutil.Option.TYPE_CHECKER["hostcolonport"]
+        self.assertIsNotNone(type_checker)
+        opt_string = option.get_opt_string(),
+        for value in values:
+            if value[1] is not None:
+                msg = "Faild to parse '%s' correctly." % value[0]
+                result = type_checker(option, opt_string, value[0])
+                self.assertEqual(result, value[1], msg)
+            else:
+                with self.assertRaises(optparse.OptionValueError):
+                    type_checker(option, opt_string, value[0])
 
-			["false", False],
-			["False", False],
-			["FaLse", False],
-			["f", False],
-			["F", False],
-			["0", False],
-			["f", False],
-			["no", False],
-			["n", False],
+    def test_check_boolean(self):
+        option = clparserutil.Option(
+            "--create",
+            action="store",
+            dest="create",
+            default=True,
+            type="boolean",
+            help="create key store - default = True")
+        values = [
+            ["true", True],
+            ["True", True],
+            ["trUe", True],
+            ["t", True],
+            ["T", True],
+            ["1", True],
+            ["y", True],
+            ["yes", True],
+            ["y", True],
 
-			["dave", None],
-			["None", None],
-			["", None],
-		]
-		type_checker = clparserutil.Option.TYPE_CHECKER["boolean"]
-		opt_string = option.get_opt_string(),
-		for value in values:
-			if value[1] is not None:
-				msg = "Faild to parse '%s' correctly." % value[0]
-				result = type_checker(option, opt_string, value[0])
-				self.assertEqual(result, value[1], msg)
-			else:
-				with self.assertRaises(optparse.OptionValueError):
-					type_checker(option, opt_string, value[0])
+            ["false", False],
+            ["False", False],
+            ["FaLse", False],
+            ["f", False],
+            ["F", False],
+            ["0", False],
+            ["f", False],
+            ["no", False],
+            ["n", False],
 
-#------------------------------------------------------------------- End-of-File
+            ["dave", None],
+            ["None", None],
+            ["", None],
+        ]
+        type_checker = clparserutil.Option.TYPE_CHECKER["boolean"]
+        opt_string = option.get_opt_string(),
+        for value in values:
+            if value[1] is not None:
+                msg = "Faild to parse '%s' correctly." % value[0]
+                result = type_checker(option, opt_string, value[0])
+                self.assertEqual(result, value[1], msg)
+            else:
+                with self.assertRaises(optparse.OptionValueError):
+                    type_checker(option, opt_string, value[0])
