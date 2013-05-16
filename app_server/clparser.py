1863a33604e4445536fce07479f0883e03884bc8
Dave Simons <simonsdave@gmail.com>
2013-05-15 21:30:57 -0400

    pep8'ing app_server/clparser.py

--- app_server/clparser.py
+++ app_server/clparser.py
@@ -1,7 +1,6 @@
 """This module contains all the logic required to parse the app
 server's command line."""
 
-#-------------------------------------------------------------------------------
 
 import re
 import logging
@@ -9,30 +8,32 @@ import optparse
 
 import clparserutil
 
-#-------------------------------------------------------------------------------
 
 class CommandLineParser(optparse.OptionParser):
 
-	def __init__(self):
-		optparse.OptionParser.__init__(
-			self,
-			"usage: %prog [options]",
-			option_class=clparserutil.Option)
+    def __init__(self):
+        optparse.OptionParser.__init__(
+            self,
+            "usage: %prog [options]",
+            option_class=clparserutil.Option)
 
-		self.add_option(
-			"--log",
-			action="store",
-			dest="logging_level",
-			default=logging.ERROR,
-			type="logginglevel",
-			help="logging level [DEBUG,INFO,WARNING,ERROR,CRITICAL,FATAL] - default = ERRROR" )
+        help = (
+            "logging level "
+            "[DEBUG,INFO,WARNING,ERROR,CRITICAL,FATAL]"
+            " - default = ERRROR"
+        )
+        self.add_option(
+            "--log",
+            action="store",
+            dest="logging_level",
+            default=logging.ERROR,
+            type="logginglevel",
+            help=help)
 
-		self.add_option(
-			"--port",
-			action="store",
-			dest="port",
-			default=8080,
-			type=int,
-			help="port - default = 8080" )
-
-#------------------------------------------------------------------- End-of-File
+        self.add_option(
+            "--port",
+            action="store",
+            dest="port",
+            default=8080,
+            type=int,
+            help="port - default = 8080")
