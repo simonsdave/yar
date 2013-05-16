0ee7c79abf3812cfafb6f487ae3f76adc8e6fca6
Dave Simons <simonsdave@gmail.com>
2013-05-15 20:42:43 -0400

    pep8'ing lib/tsh.py

--- lib/tsh.py
+++ lib/tsh.py
@@ -2,25 +2,21 @@
 SIGINT handler so that when an app is halted using a CTRL+C it looks
 like a clean shutdown."""
 
+
 import signal
 import sys
 import logging
 
-#-------------------------------------------------------------------------------
 
 _logger = logging.getLogger("UTIL.%s" % __name__)
 
-#-------------------------------------------------------------------------------
 
 def _term_signal_handler(signalNumber, frame):
-	assert signalNumber == signal.SIGINT
-	_logger.info("Shutting down ...")
-	sys.exit(0)
+    assert signalNumber == signal.SIGINT
+    _logger.info("Shutting down ...")
+    sys.exit(0)
 
-#-------------------------------------------------------------------------------
 
 def install_handler():
-	"""Install this module's SIGTERM handler."""
-	signal.signal(signal.SIGINT, _term_signal_handler)
-
-#------------------------------------------------------------------- End-of-File
+    """Install this module's SIGTERM handler."""
+    signal.signal(signal.SIGINT, _term_signal_handler)
