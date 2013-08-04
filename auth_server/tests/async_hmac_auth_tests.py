6275b1f1db18607f9765418f430ef515987f50cb
Dave Simons <simonsdave@gmail.com>
2013-08-04 11:26:22 -0400

    Adding unit test case async_hmac_auth auth server's async_hmac_auth module to validate authentication fails in the expected manner when a request is made with a mac key identifier that's unknown to key server. Dave.

--- auth_server/tests/async_hmac_auth_tests.py
+++ auth_server/tests/async_hmac_auth_tests.py
@@ -175,3 +175,57 @@ class TestAsyncHMACAuth(yar_test_util.TestCase):
                 request=request,
                 generate_debug_headers=False)
             aha.validate(on_auth_done)
+
+    def test_creds_not_found(self):
+        """When a request contains Authorization HTTP header with a
+        mac key identifier that the key server doesn't know about,
+        confirm that ```async_hmac_auth.AsyncHMACAuth``` tags this as an
+        authorization failure with
+        ```async_hmac_auth.AUTH_FAILURE_DETAIL_CREDS_NOT_FOUND```
+        detailed error code."""
+
+        def on_auth_done(is_auth_ok, auth_failure_detail):
+
+            self.assertIsNotNone(is_auth_ok)
+            self.assertFalse(is_auth_ok)
+
+            self.assertIsNotNone(auth_failure_detail)
+            self.assertEqual(
+                auth_failure_detail,
+                async_hmac_auth.AUTH_FAILURE_DETAIL_CREDS_NOT_FOUND)
+
+        def async_nonce_checker_fetch_patch(
+            ignore_async_nonce_checker,
+            callback,
+            mac_key_identifier,
+            nonce):
+            callback(True)
+
+        name_of_method_to_patch = "async_nonce_checker.AsyncNonceChecker.fetch"
+        with mock.patch(name_of_method_to_patch, async_nonce_checker_fetch_patch):
+
+            def async_creds_retriever_fetch_patch(
+                ignore_async_creds_retriever,
+                callback,
+                mac_key_identifier):
+                callback(False, mac_key_identifier)
+
+            name_of_method_to_patch = "async_creds_retriever.AsyncCredsRetriever.fetch"
+            with mock.patch(name_of_method_to_patch, async_creds_retriever_fetch_patch):
+
+                auth_header_value = mac.AuthHeaderValue(
+                    mac_key_identifier=mac.MACKeyIdentifier.generate(),
+                    ts=mac.Timestamp.generate(),
+                    nonce=mac.Nonce.generate(),
+                    ext=mac.Ext.generate(content_type=None, body=None),
+                    mac=mac.MAC("0123456789"))
+
+                request = mock.Mock()
+                request.headers = tornado.httputil.HTTPHeaders({
+                    "Authorization": str(auth_header_value),
+                })
+
+                aha = async_hmac_auth.AsyncHMACAuth(
+                    request=request,
+                    generate_debug_headers=False)
+                aha.validate(on_auth_done)
