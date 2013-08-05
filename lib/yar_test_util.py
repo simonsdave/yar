0039cccc3d206e333866d17ae51211a2436b5c1d
Dave Simons <simonsdave@gmail.com>
2013-08-05 18:40:40 -0400

    Consolidating test utilities that used to be in lib/tests/testutil.py **and** lib/yar_test_util.py into just the later. Dave.

--- lib/yar_test_util.py
+++ lib/yar_test_util.py
@@ -1,10 +1,60 @@
 """This module implements a collection of utilities for unit testing yar"""
 
 import json
+import socket
+import threading
 import unittest
 import uuid
 
+import tornado.ioloop
+import tornado.netutil
+
 import jsonschema
+
+
+class Server(object):
+    """An abstract base class for mock auth server, key server and
+    app server. The primary reason for this class to exist is so the
+    constructor can find an available port for the server to run and
+    save that port & associated socket object in the socket and
+    port properties."""
+
+    def __init__(self):
+        """Opens a random but available socket and assigns it to the
+        socket property. The socket's port is also assigned to the
+        port property."""
+        object.__init__(self)
+
+        [self.socket] = tornado.netutil.bind_sockets(
+            0,
+            "localhost",
+            family=socket.AF_INET)
+        self.port = self.socket.getsockname()[1]
+
+    def shutdown(self):
+        """Can be overriden by derived classes to perform server
+        type specific shutdown. This method is a no-op but derived
+        classes should call this method in case this is not a no-op
+        in the future."""
+        pass
+
+
+class IOLoop(threading.Thread):
+    """This class makes it easy for a test case's `setUpClass()` to start
+    a Tornado io loop on the non-main thread so that the io loop, auth server
+    key server and app server can operate 'in the background' while the
+    unit test runs on the main thread."""
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.daemon = True
+
+    def run(self):
+        tornado.ioloop.IOLoop.instance().start()
+
+    def stop(self):
+        tornado.ioloop.IOLoop.instance().stop()
+
 
 class TestCase(unittest.TestCase):
 
