aee1e1df444a919928d05c0eab05ad46404997e8
Dave Simons <simonsdave@gmail.com>
2013-09-01 11:52:10 -0400

    Adding unit tests to key server which verify async_creds_retriever's behaviour when retrieving all credentials

--- key_server/tests/async_creds_retriever_unit_tests.py
+++ key_server/tests/async_creds_retriever_unit_tests.py
@@ -279,3 +279,90 @@ class TestCaseAsyncCredsRetriever(yar_test_util.TestCase):
             the_is_filter_out_deleted=True,
             the_is_filter_out_non_model_properties=False)
 
+    def test_ok_on_retrieve_all_request_to_key_store(self):
+        """_test_ok_on_owner_request_to_key_store tests the bulk
+        of this functionality - all the test is really here to do
+        is confirm that the correct request is issued to the
+        key server."""
+
+        the_body = {
+            "offset": 0, 
+            "rows": [
+                {
+                    "id": "070a69935bd840e09029a74837dc4755", 
+                    "key": "070a69935bd840e09029a74837dc4755", 
+                    "value": {
+                        "_id": "070a69935bd840e09029a74837dc4755", 
+                        "_rev": "1-f1a30395b5df30003421a8969b001be7", 
+                        "is_deleted": False, 
+                        "mac_algorithm": "hmac-sha-1", 
+                        "mac_key": "CHlFh7fIejF3NLmr73pCfqx3EL_xV2zDQgVcjRl45jM", 
+                        "mac_key_identifier": "070a69935bd840e09029a74837dc4755", 
+                        "owner": "dave@gmail.com", 
+                        "type": "cred_v1.0"
+                    }
+                }, 
+                {
+                    "id": "9c8411a78405460e825b5f4318ef9a57", 
+                    "key": "9c8411a78405460e825b5f4318ef9a57", 
+                    "value": {
+                        "_id": "9c8411a78405460e825b5f4318ef9a57", 
+                        "_rev": "1-ee4cfcd905e7d98ee7d0ed30463a7a99", 
+                        "is_deleted": False, 
+                        "mac_algorithm": "hmac-sha-1", 
+                        "mac_key": "pyHpZ6auevEEHTkZDhfVf-uKHV_YPEH2lctZXL5j4QQ", 
+                        "mac_key_identifier": "9c8411a78405460e825b5f4318ef9a57", 
+                        "owner": "dave@gmail.com", 
+                        "type": "cred_v1.0"
+                    }
+                }, 
+                {
+                    "id": "e49df12a968f4340a6e5f06c43e868bb", 
+                    "key": "e49df12a968f4340a6e5f06c43e868bb", 
+                    "value": {
+                        "_id": "e49df12a968f4340a6e5f06c43e868bb", 
+                        "_rev": "1-8cfdd40a34ecaf4944e790e6beb028c5", 
+                        "is_deleted": False, 
+                        "mac_algorithm": "hmac-sha-1", 
+                        "mac_key": "8m_yH35Q9iMEZx_OV5M136eJ4SJsTw0JO-Y6BS3scJY", 
+                        "mac_key_identifier": "e49df12a968f4340a6e5f06c43e868bb", 
+                        "owner": "dave@gmail.com", 
+                        "type": "cred_v1.0"
+                    }
+                }
+            ], 
+            "total_rows": 3
+        }
+
+        def async_req_to_key_store_patch(
+            acr,
+            path,
+            method,
+            body,
+            callback):
+
+            self.assertIsNotNone(acr)
+
+            self.assertIsNotNone(path)
+            self.assertEqual(path, "_design/creds/_view/all")
+
+            self.assertIsNotNone(method)
+            self.assertEqual(method, "GET")
+
+            self.assertIsNone(body)
+
+            self.assertIsNotNone(callback)
+            callback(is_ok=True, code=httplib.OK, body=the_body)
+
+        def on_async_create_done(creds, is_creds_collection):
+            pass
+
+        name_of_method_to_patch = "ks_util.AsyncAction.async_req_to_key_store"
+        with mock.patch(name_of_method_to_patch, async_req_to_key_store_patch):
+            acr = async_creds_retriever.AsyncCredsRetriever(type(self)._key_store)
+            acr.fetch(
+                callback=on_async_create_done,
+                mac_key_identifier=None,
+                owner=None,
+                is_filter_out_deleted=False,
+                is_filter_out_non_model_properties=False)
