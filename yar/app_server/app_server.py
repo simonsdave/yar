cca1dfaf4e3dee597ba442ab977d59d2a60dfca9
Dave Simons <simonsdave@gmail.com>
2013-11-18 20:34:41 -0500

    git lost it's mind - these files aren't really new

---
+++ yar/app_server/app_server.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+"""This module contains a super simple app server to be used for testing."""
+
+import logging
+import os
+import sys
+import datetime
+import uuid
+import httplib
+
+from dasutils import tsh
+import tornado.httpserver
+import tornado.httpclient
+import tornado.ioloop
+import tornado.web
+
+from clparser import CommandLineParser
+
+
+__version__ = "1.0"
+
+
+_logger = logging.getLogger("APPSERVER.%s" % __name__)
+
+
+class RequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        auth_hdr_value = self.request.headers.get(
+            "Authorization",
+            "<no auth header>")
+        dict = {
+            "status": "ok",
+            "version": __version__,
+            "when": str(datetime.datetime.now()),
+            "auth": auth_hdr_value,
+        }
+        self.write(dict)
+
+    def post(self):
+        location_url = "%s/%s" % (
+            self.request.full_url(),
+            str(uuid.uuid4()).replace("-", ""))
+        self.set_header("Location", location_url)
+        self.set_status(httplib.CREATED)
+        self.get()
+
+    def put(self):
+        self.post()
+
+    def delete(self):
+        self.get()
+
+    def head(self):
+        self._get(False)
+
+    def options(self):
+        self.get()
+
+
+if __name__ == "__main__":
+    clp = CommandLineParser()
+    (clo, cla) = clp.parse_args()
+
+    logging.basicConfig(level=clo.logging_level)
+
+    tsh.install()
+
+    _logger.info(
+        "%s %s running on %d",
+        os.path.basename(os.path.split(sys.argv[0])[1]),
+        __version__,
+        clo.port)
+
+    app = tornado.web.Application(handlers=[(r".*", RequestHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(clo.port)
+    tornado.ioloop.IOLoop.instance().start()
---
+++ yar/app_server/clparser.py
@@ -0,0 +1,41 @@
+"""This module contains all the logic required to parse the app
+server's command line."""
+
+import logging
+import optparse
+
+from dasutils import clparserutil
+
+
+class CommandLineParser(optparse.OptionParser):
+
+    def __init__(self):
+        optparse.OptionParser.__init__(
+            self,
+            "usage: %prog [options]",
+            option_class=clparserutil.Option)
+
+        default = logging.ERROR
+        fmt = (
+            "logging level "
+            "[DEBUG,INFO,WARNING,ERROR,CRITICAL,FATAL]"
+            " - default = %s"
+        )
+        help = fmt % logging.getLevelName(default)
+        self.add_option(
+            "--log",
+            action="store",
+            dest="logging_level",
+            default=default,
+            type="logginglevel",
+            help=help)
+
+        default = 8080
+        help="port - default = %d" % default
+        self.add_option(
+            "--port",
+            action="store",
+            dest="port",
+            default=default,
+            type=int,
+            help=help)
