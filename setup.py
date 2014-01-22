4dcd74d6e8c919bed4fb799adbe1389b91ff45ea
Dave Simons <simonsdave@gmail.com>
2014-01-21 20:12:38 -0500

    first cut at setup.py for yar

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="yar",
+    packages=[
+        "yar",
+        "yar.util",
+        "yar.app_server",
+        "yar.auth_server",
+        "yar.key_server",
+        "yar.key_store",
+    ],
+    scripts=[
+        "bin/app_server",
+        "bin/auth_server",
+        "bin/key_server",
+        "bin/key_server_installer",
+    ],
+    install_requires=[
+        "httplib2==0.8",
+        "jsonschema==1.3.0",
+        "tornado==3.0.1",
+        "python-keyczar==0.71c",
+        "python-memcached==1.52",
+        "tornado-memcache==1.0",
+    ],
+    dependency_links = [
+        "http://github.com/dpnova/tornado-memcache/tarball/master",
+    ],
+    version=1.0,
+    description="yar",
+    author="Dave Simons",
+    author_email="simonsdave@gmail.com",
+    url="https://github.com/simonsdave/yar"
+)
