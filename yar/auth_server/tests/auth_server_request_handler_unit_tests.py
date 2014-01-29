2f1fd7cb7470f4fa38e4ac5a711cfe0a15064d41
Dave Simons <simonsdave@gmail.com>
2014-01-28 22:02:43 -0500

    improving readability of auth server's unit tests

--- yar/auth_server/tests/auth_server_request_handler_unit_tests.py
+++ yar/auth_server/tests/auth_server_request_handler_unit_tests.py
@@ -16,6 +16,7 @@ import tornado.testing
 
 from yar.tests import yar_test_util
 from yar.auth_server import auth_server_request_handler
+from yar.auth_server.auth_server_request_handler import auth_failure_detail_header_name
 
 
 class AuthServer(yar_test_util.Server):
@@ -78,6 +79,7 @@ class DebugHeadersDict(dict):
 
 
 class MyTestCase(tornado.testing.AsyncHTTPTestCase):
+    """Unit tests for ```auth_server_request_handler.RequestHandler```."""
 
     def get_app(self):
         handlers = [
@@ -89,30 +91,41 @@ class MyTestCase(tornado.testing.AsyncHTTPTestCase):
         self.app = tornado.web.Application(handlers=handlers)
         return self.app
 
-    @classmethod
-    def setUpClass(cls):
-        auth_server_request_handler._include_auth_failure_debug_details = True
+    def _get_auth_failure_detail(self, response):
+        """Search thru ```response```'s headers and if the
+        auth failure detail header is found return a tuple
+        that's the auth failure detail header's name and value."""
+        for key, value in response.headers.iteritems():
+            if auth_failure_detail_header_name.lower() == key.lower():
+                return (key, value)
+        return (None, None)
 
-    @classmethod
-    def tearDownClass(cls):
-        auth_server_request_handler._include_auth_failure_debug_details = False
-
-    def _assertAuthorizationFailureDetail(self, response, auth_failure_detail):
+    def assertAuthFailureDetail(self, response, auth_failure_detail):
         """Assert an authorization failure detail HTTP header appears
         in ```response``` with a value equal to ```auth_failure_detail```."""
-        for key, value in response.headers.iteritems():
-            if auth_server_request_handler.auth_failure_detail_header_name.lower() == key.lower():
-                self.assertEqual(int(value), auth_failure_detail)
-                return
+        (key, value) = self._get_auth_failure_detail(response)
+        self.assertIsNotNone(key)
+        self.assertIsNotNone(value)
+        self.assertEqual(int(value), auth_failure_detail)
 
-        self.assertTrue(False)
+    def assertNoAuthFailureDetail(self, response):
+        """Assert *no* authorization failure detail HTTP header
+        appears in ```response```."""
+        (key, value) = self._get_auth_failure_detail(response)
+        self.assertIsNone(key)
+        self.assertIsNone(value)
 
     def test_no_authorization_header(self):
         """This test confirms that authentication fails if no Authorization
         header is supplied in the auth server's."""
+
+        # setting _include_auth_failure_debug_details to True forces
+        # the auth server to include auth failure details in response
+        auth_server_request_handler._include_auth_failure_debug_details = True
+
         response = self.fetch("/", method="GET", headers={})
         self.assertEqual(response.code, httplib.UNAUTHORIZED)
-        self._assertAuthorizationFailureDetail(
+        self.assertAuthFailureDetail(
             response,
             auth_server_request_handler.AUTH_FAILURE_DETAIL_NO_AUTH_HEADER)
 
@@ -120,16 +133,22 @@ class MyTestCase(tornado.testing.AsyncHTTPTestCase):
         """This test confirms that authentication fails if an Authorization
         header is supplied but it contains an unrecognized authententication
         scheme."""
+
+        # setting _include_auth_failure_debug_details to True forces
+        # the auth server to include auth failure details in response
+        auth_server_request_handler._include_auth_failure_debug_details = True
+
         response = self.fetch("/", method="GET", headers={"Authorization": "DAVE"})
         self.assertEqual(response.code, httplib.UNAUTHORIZED)
-        self._assertAuthorizationFailureDetail(
+        self.assertAuthFailureDetail(
             response,
             auth_server_request_handler.AUTH_FAILURE_DETAIL_UNKNOWN_AUTHENTICATION_SCHEME)
 
-    def test_hmac_auth_failed_auth_failure_detail_in_auth_server_response(self):
+    def test_auth_failure_detail_correctly_in_auth_server_response(self):
         """This test confirms that when an authenticator
         supplies authentication failure detail that the failure
-        detail is in the auth server's HTTP response as an HTTP header."""
+        detail is in the auth server's HTTP response as an HTTP header
+        if the appropriate configuration is enabled."""
 
         the_auth_failure_detail = auth_server_request_handler.AUTH_FAILURE_DETAIL_FOR_TESTING
 
@@ -141,9 +160,25 @@ class MyTestCase(tornado.testing.AsyncHTTPTestCase):
             "async_hmac_auth.AsyncHMACAuth.authenticate"
         )
         with mock.patch(name_of_method_to_patch, authenticate_patch):
+            # step #1 ...
+
+            # setting _include_auth_failure_debug_details to True forces
+            # the auth server to include auth failure details in response
+            auth_server_request_handler._include_auth_failure_debug_details = True
+
             response = self.fetch("/", method="GET", headers={"Authorization": "MAC ..."})
             self.assertEqual(response.code, httplib.UNAUTHORIZED)
-            self._assertAuthorizationFailureDetail(response, the_auth_failure_detail)
+            self.assertAuthFailureDetail(response, the_auth_failure_detail)
+
+            # step #2 ...
+
+            # setting _include_auth_failure_debug_details to False forces
+            # the auth server to *not* include auth failure details in response
+            auth_server_request_handler._include_auth_failure_debug_details = False
+
+            response = self.fetch("/", method="GET", headers={"Authorization": "MAC ..."})
+            self.assertEqual(response.code, httplib.UNAUTHORIZED)
+            self.assertNoAuthFailureDetail(response)
 
 
 class TestCase(yar_test_util.TestCase):
@@ -165,7 +200,7 @@ class TestCase(yar_test_util.TestCase):
         cls.auth_server.shutdown()
         cls.auth_server = None
 
-    def assertAuthorizationFailureDetail(
+    def assertAuthFailureDetail(
         self,
         response,
         auth_failure_detail=None):
@@ -197,48 +232,6 @@ class TestCase(yar_test_util.TestCase):
             self.assertTrue(0 == len(debug_headers_in_response))
         else:
             self.assertTrue(debug_headers_in_response.is_equal(debug_headers))
-
-    def test_hmac_auth_failed(self):
-        """Verify that when async authentication fails, the response
-        is ```httplib.UNAUTHORIZED```.
-
-        This test will also confirm that when the authenication
-        mechanism fails to supply authentication failure detail,
-        no authentication failure detail HTTP header is included
-        in the response.
-
-        Fianlly, this tests verifies that an authenication failure
-        which generates no debug headers results in an HTTP response
-        from the auth server with no debug headers."""
-        def authenticate_patch(an_async_hmac_auth, callback):
-            self.assertIsNotNone(an_async_hmac_auth)
-            self.assertIsNotNone(callback)
-            callback(is_auth_ok=False)
-
-        name_of_method_to_patch = (
-            "yar.auth_server.hmac."
-            "async_hmac_auth.AsyncHMACAuth.authenticate"
-        )
-        with mock.patch(name_of_method_to_patch, authenticate_patch):
-            def forward_patch(ignore_async_app_server_forwarder, callback):
-                """This should never be called when authentication fails."""
-                self.assertTrue(False)
-
-            name_of_method_to_patch = (
-                "yar.auth_server."
-                "async_app_server_forwarder.AsyncAppServerForwarder.forward"
-            )
-            with mock.patch(name_of_method_to_patch, forward_patch):
-                http_client = httplib2.Http()
-                response, content = http_client.request(
-                    "http://localhost:%d/whatever" % type(self).auth_server.port,
-                    "GET",
-                    headers={"Authorization": "MAC ..."})
-                self.assertIsNotNone(response)
-                self.assertIsNotNone(response.status)
-                self.assertEqual(response.status, httplib.UNAUTHORIZED)
-                self.assertAuthorizationFailureDetail(response)
-                self.assertAuthorizationDebugHeaders(response)
 
     @unittest.skip("only for a wee bit")
     def test_auth_failure_debug_details_in_auth_server_response(self):
@@ -282,7 +275,7 @@ class TestCase(yar_test_util.TestCase):
                 self.assertIsNotNone(response)
                 self.assertIsNotNone(response.status)
                 self.assertEqual(response.status, httplib.UNAUTHORIZED)
-                self.assertAuthorizationFailureDetail(response)
+                self.assertAuthFailureDetail(response)
                 self.assertAuthorizationDebugHeaders(
                     response,
                     the_auth_failure_debug_details)
