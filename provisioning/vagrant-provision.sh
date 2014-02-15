eda895722b8abc5417d0d477a8f20133278becca
Dave Simons <simonsdave@gmail.com>
2014-02-15 07:49:55 -0500

    add apache benchmark for stress testing

--- provisioning/vagrant-provision.sh
+++ provisioning/vagrant-provision.sh
@@ -5,6 +5,9 @@ apt-get update
 # curl's a generally useful utility across SO many platforms ...
 apt-get install -y curl
 
+# for apache benchmark
+apt-get install -y apache2-utils
+
 # http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-precise-12-04-lts-64-bit
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
