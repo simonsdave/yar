eb592cca8f7f4214bf1c03c48ba9f03ab1387729
Dave Simons <simonsdave@gmail.com>
2014-02-17 09:54:11 -0500

    include libmemcached-tools to poke @ nonce store

--- provisioning/vagrant-provision.sh
+++ provisioning/vagrant-provision.sh
@@ -9,6 +9,9 @@ apt-get install -y curl
 # http://httpd.apache.org/docs/2.2/programs/ab.html
 apt-get install -y apache2-utils
 
+# simple command line tools for poking @ memcached
+apt-get install -y libmemcached-tools
+
 # http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-precise-12-04-lts-64-bit
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
