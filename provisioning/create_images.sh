37366fce109d36a04b412348d8141b2725799fd5
Dave Simons <simonsdave@gmail.com>
2014-02-16 21:12:11 -0500

    fixed script creating docker images for testing

--- provisioning/create_images.sh
+++ provisioning/create_images.sh
@@ -4,18 +4,16 @@ SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
 cd $SCRIPT_DIR_NAME
 
+pushd Nonce-Store
+sudo docker build -t nonce_store_img .
+popd
+
 pushd Key-Store
 sudo docker build -t key_store_img .
 popd
 
-exit 0
-
 pushd yar
 sudo docker build -t yar_img .
-popd
-
-pushd Nonce-Store
-sudo docker build -t key_store_img .
 popd
 
 exit 0
