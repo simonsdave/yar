5466037ee56188a1218f84e4d3794d61aab1f49a
Dave Simons <simonsdave@gmail.com>
2014-02-21 06:20:02 -0500

    adding :TODO: reminder

--- integration_tests/vagrant-provision.sh
+++ integration_tests/vagrant-provision.sh
@@ -12,6 +12,8 @@ apt-get install -y apache2-utils
 # simple command line tools for poking @ memcached
 apt-get install -y libmemcached-tools
 
+# :TODO: reminder - docker recommends updating linux kernel - who to do this?
+
 # http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-precise-12-04-lts-64-bit
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
