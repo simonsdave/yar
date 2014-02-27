05abf8ee733f23088d5c1e9e014471356ec19b18
Dave Simons <simonsdave@gmail.com>
2014-02-27 06:59:40 -0500

    good step parameterizing load test scripts

--- integration_tests/run_load_test.sh
+++ integration_tests/run_load_test.sh
@@ -20,14 +20,21 @@ get_deployment_config() {
 }
 
 take_95th_percentile() {
-	x=$(tail -n +2 $SCRIPT_DIR_NAME/load_test_results.tsv | wc -l)
+    local INPUT_FILENAME=${1:-}
+    local OUTPUT_FILENAME=${2:-}
+	x=$(tail -n +2 $INPUT_FILENAME | wc -l)
 	n=$(python -c "print int($x * .95)")
-	tmpfile=$(mktemp)
-	sort --field-separator=$'\t' --key=5 -n $SCRIPT_DIR_NAME/load_test_results.tsv | head -n $n | sort --field-separator=$'\t' --key=2 -n > $tmpfile
-	mv $tmpfile $SCRIPT_DIR_NAME/load_test_results.tsv
+	sort \
+        --field-separator=$'\t' \
+        --key=5 \
+        -n $INPUT_FILENAME | \
+    head \
+        -n $n | \
+    sort \
+        --field-separator=$'\t' \
+        --key=2 \
+        -n > $OUTPUT_FILENAME
 }
-
-rm -f load_test_results.* >& /dev/null
 
 # :TODO: what if either of these scripts fail?
 $SCRIPT_DIR_NAME/rm_all_containers.sh
@@ -36,9 +43,38 @@ $SCRIPT_DIR_NAME/spin_up_deployment.sh
 API_KEY=$(get_deployment_config "API_KEY")
 # :TODO: what if API_KEY doesn't exist?
 
-ab -c 1 -n 2500 -A $API_KEY: -g load_test_results.tsv http://172.17.0.7:8000/dave.html
-$SCRIPT_DIR_NAME/plot_load_test_results
-mv $SCRIPT_DIR_NAME/load_test_results.jpg $SCRIPT_DIR_NAME/load_test_results_1x2500.jpg
+rm -f load_test_results* >& /dev/null
+
+NUMBER_OF_REQUESTS=2500
+NUMBER_OF_REQUESTS=1000
+PERCENTILE=95
+
+CONCURRENCY=1
+LOAD_TEST_RESULTS_DATA=$SCRIPT_DIR_NAME/load_test_results-$CONCURRENCY-$NUMBER_OF_REQUESTS.tsv
+LOAD_TEST_RESULTS_DATA_PERCENTILE=$SCRIPT_DIR_NAME/load_test_results-$CONCURRENCY-$NUMBER_OF_REQUESTS-$PERCENTILE.tsv
+LOAD_TEST_RESULTS_PLOT=$SCRIPT_DIR_NAME/load_test_results-$CONCURRENCY-$NUMBER_OF_REQUESTS.jpg
+
+ab \
+    -c $CONCURRENCY \
+    -n $NUMBER_OF_REQUESTS \
+    -A $API_KEY: \
+    -g $LOAD_TEST_RESULTS_DATA \
+    http://172.17.0.7:8000/dave.html
+
+take_95th_percentile \
+    $LOAD_TEST_RESULTS_DATA \
+    $LOAD_TEST_RESULTS_DATA_PERCENTILE
+
+gnuplot \
+    -e "input_filename='$LOAD_TEST_RESULTS_DATA_PERCENTILE'" \
+    -e "title='Concurrency = $CONCURRENCY; Number of Requests = $NUMBER_OF_REQUESTS; Percentile = $PERCENTILE th'" \
+    -e "output_filename='$LOAD_TEST_RESULTS_PLOT'" \
+    $SCRIPT_DIR_NAME/plot_load_test_results
+
+convert \
+    $SCRIPT_DIR_NAME/*.jpg \
+    $SCRIPT_DIR_NAME/load_test_results.pdf
+exit 0
 
 ab -c 10 -n 2500 -A $API_KEY: -g load_test_results.tsv http://172.17.0.7:8000/dave.html
 take_95th_percentile
