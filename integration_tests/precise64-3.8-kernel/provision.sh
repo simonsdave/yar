7ccd466563b671b1aefeb374d7a666cbfb867e51
Dave Simons <simonsdave@gmail.com>
2014-03-16 13:14:03 -0400

    enable cgroup memory metrics collection

--- integration_tests/precise64-3.8-kernel/provision.sh
+++ integration_tests/precise64-3.8-kernel/provision.sh
@@ -5,4 +5,11 @@ apt-get update
 apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
 apt-get install -y virtualbox-guest-utils
 
-sudo shutdown -r now
+# based on http://docs.docker.io/en/latest/installation/ubuntulinux/
+# tell kernel to collect memory metrics - this is disabled by default
+#
+# for a more complete story on container monitoring see
+# http://blog.docker.io/2013/10/gathering-lxc-docker-containers-metrics/
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/g' /etc/default/grub
+
+shutdown -r now
