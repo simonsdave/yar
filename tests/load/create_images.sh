bf7b74e51be25f48bc95b5c1986218e36f9f5333
Dave Simons <simonsdave@gmail.com>
2014-03-16 13:27:47 -0400

    rename tests/load/dave -> tests/load/couchdb

--- tests/load/create_images.sh
+++ tests/load/create_images.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
 sudo docker build -t haproxy_img $SCRIPT_DIR_NAME/haproxy
 sudo docker build -t memcached_img $SCRIPT_DIR_NAME/memcached
-sudo docker build -t couchdb_img $SCRIPT_DIR_NAME/CouchDB
+sudo docker build -t couchdb_img $SCRIPT_DIR_NAME/couchdb
 sudo docker build -t yar_img $SCRIPT_DIR_NAME/yar
 
 exit 0
