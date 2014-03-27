9a9ebb7e42603c258c03e4709ead2343f3a27c63
Dave Simons <simonsdave@gmail.com>
2014-03-27 06:57:58 -0400

    iteration of JS to randomly select load test creds

--- tests/load/lots-of-creds/load_test.js
+++ tests/load/lots-of-creds/load_test.js
@@ -2,7 +2,7 @@
 	"language": "javascript",
 	"views": {
 		"random_set_of_creds": {
-			"map": "function(doc) { if (doc.type.match(/^creds_v\\d+.\\d+/i)) var api_key = doc.basic.api_key; st = api_key.substr(api_key.length - 2, 2); x = parseInt(\"0x\" + st); key = Math.floor( (99.0 * x) / 255 ); emit(key, doc) }",
+			"map": "function(doc) { if (doc.type.match(/^creds_v\\d+.\\d+/i)) var api_key = doc.basic.api_key; var num_digits = 4; var digits = \"0x\" + api_key.substr(api_key.length - num_digits, num_digits); var div = Math.pow(2.0, num_digits * 8) - 1; var key = Math.floor((99.0 * parseInt(digits)) / 0xffff); emit(key, doc) }",
             "reduce": "_count"
 		}
 	}
--- tests/load/lots-of-creds/load_test.sh
+++ tests/load/lots-of-creds/load_test.sh
@@ -9,7 +9,6 @@ echo "Creating Key Store"
 DATA_DIRECTORY=$(mktemp -d)
 EXISTING_CREDS=$SCRIPT_DIR_NAME/50000.creds.couch
 if ! KEY_STORE=$(create_key_store $DATA_DIRECTORY $EXISTING_CREDS); then
-# if ! KEY_STORE=$(create_key_store $DATA_DIRECTORY ""); then
     echo "Failed to create key store"
     exit 1
 fi
