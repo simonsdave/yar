fb7d849e27361dfaae5c2e3cdbf2d7d8570f2785
Dave Simons <simonsdave@gmail.com>
2014-03-30 10:29:25 -0400

    getting locustfile timestamping right

--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -1,9 +1,13 @@
+import time
+
 from locust import HttpLocust
 from locust import task
 from locust import TaskSet
 from requests.auth import HTTPBasicAuth
 
 # locust -f ./locustfile.py -H http://172.17.0.8:8000 --no-web -n 10000 -c 25 -r 5 --logfile=./ooo
+
+_start_time = time.time()
 
 class Behavior(TaskSet):
     min_wait = 0
@@ -13,8 +17,11 @@ class Behavior(TaskSet):
     def get_request(self):
         response = self.client.get(
             "/dave-was-here.html",
-            auth=HTTPBasicAuth("b2b3125dd6964307810cdcf27764aad0", ""))
-        print ">>>%s<<<>>>%s<<<" % (response.status_code, response.elapsed.total_seconds())
+            auth=HTTPBasicAuth("753786be2dda4da6b4ea13d5029ddfcc", ""))
+        print "<<<%s>>>>>>%s<<<>>>%s<<<" % (
+			int(round(time.time() - _start_time)),
+			response.status_code,
+			response.elapsed.total_seconds())
 
 
 class User(HttpLocust):
