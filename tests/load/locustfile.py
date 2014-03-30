5976dea85f85a3f241e9a683f73c459572ffcfbb
Dave Simons <simonsdave@gmail.com>
2014-03-30 07:42:05 -0400

    closer to "right" basic load test locustfile.py

--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -1,18 +1,21 @@
 from locust import HttpLocust
+from locust import task
 from locust import TaskSet
 from requests.auth import HTTPBasicAuth
 
-# locust -f ./locustfile.py -H http://172.17.0.9:8000 --no-web -n 100 -c 10 --logfile=./ooo
+# locust -f ./locustfile.py -H http://172.17.0.8:8000 --no-web -n 10000 -c 25 -r 5 --logfile=./ooo
 
 class Behavior(TaskSet):
-    def on_start(self):
+    min_wait = 0
+    max_wait = 0
+
+    @task
+    def get_request(self):
         response = self.client.get(
             "/dave-was-here.html",
-            auth=HTTPBasicAuth("8ca21f9dd6ea404d912a518263bc2b58", ""))
+            auth=HTTPBasicAuth("b2b3125dd6964307810cdcf27764aad0", ""))
         print ">>>%s<<<>>>%s<<<" % (response.status_code, response.elapsed.total_seconds())
 
 
 class User(HttpLocust):
     task_set = Behavior
-    min_wait = 0
-    max_wait = 0
