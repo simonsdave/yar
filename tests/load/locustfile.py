f339e6178877a1b713ca6bfc332eb42343ca8895
Dave Simons <simonsdave@gmail.com>
2014-04-01 19:41:32 -0400

    locustfile.py using randomly gen creds

--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -1,3 +1,7 @@
+# locust -f ./locustfile.py -H http://172.17.0.8:8000 --no-web -n 10000 -c 25 -r 5 --logfile=./ooo
+
+import os
+import random
 import time
 
 from locust import HttpLocust
@@ -5,9 +9,11 @@ from locust import task
 from locust import TaskSet
 from requests.auth import HTTPBasicAuth
 
-# locust -f ./locustfile.py -H http://172.17.0.8:8000 --no-web -n 10000 -c 25 -r 5 --logfile=./ooo
+with open(os.path.expanduser("~/.yar.creds.random.set"), "r") as f:
+    _api_keys = [line.strip() for line in f]
 
 _start_time = time.time()
+
 
 class Behavior(TaskSet):
     min_wait = 0
@@ -15,13 +21,14 @@ class Behavior(TaskSet):
 
     @task
     def get_request(self):
+        api_key = _api_keys[int(random.uniform(0, len(_api_keys) - 1))]
         response = self.client.get(
             "/dave-was-here.html",
-            auth=HTTPBasicAuth("753786be2dda4da6b4ea13d5029ddfcc", ""))
+            auth=HTTPBasicAuth(api_key, ""))
         print "<<<%s>>>>>>%s<<<>>>%s<<<" % (
-			int(round(time.time() - _start_time)),
-			response.status_code,
-			response.elapsed.total_seconds())
+            int(round(time.time() - _start_time)),
+            response.status_code,
+            response.elapsed.total_seconds())
 
 
 class User(HttpLocust):
