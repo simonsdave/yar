d21e95dc3987c63ca821c0f13112e62b2acc27a5
Dave Simons <simonsdave@gmail.com>
2015-12-19 19:33:34 -0500

    requests 2.8.1 -> requests

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         "httplib2==0.9.2",
         "jsonschema==2.5.1",
         "python-keyczar==0.715",
-        "requests==2.8.1",
+        "requests==2.9.0",
         "tornado==4.3",
         "tornado-memcache==0.1",
     ],
